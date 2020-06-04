ed831bbc8928a88c990efe1a450e7d936c0a2054
Mayank <immayankvrm>
2020-06-04 05:52:20 +0530

    random quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def primary():
-     print("Keep it logically awesome.")
+     #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+     f = open("quotes.txt")
+     quotes = f.readlines()
+     f.close()
+     import random
 
-  #print(quotes)
+     print(quotes[random.randint(0,13)])
 
 if __name__== "__main__":
   primary()
